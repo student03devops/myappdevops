27b4d12c8548bf4989cc554ce2f6646d07f81c7b
Student03 <student03@examplecorp.es>
2019-04-10 12:46:38 +0000

    change welcome text

--- src/main/java/es/examplecorp/HelloWorld.java
+++ src/main/java/es/examplecorp/HelloWorld.java
@@ -27,7 +27,7 @@ public class HelloWorld implements SparkApplication {
         map.put("Name", text);
 
 
-        get("/",(req,res) -> "hello DevOps");
+        get("/",(req,res) -> "hello DevOps: This is fort apache!");
 
         // Hello World with Template
         get("/hello",(req,res) -> new ModelAndView(map,"hello.hbs"),new HandlebarsTemplateEngine());
